4101cb3cde4cec877e25d8e29c61d5bbc45bc60e
Amanda Davi <a.dos.santos.davi@avanade.com>
2024-01-10 10:52:13 -0300

    complete all

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("================= Meu Iphone ====================");
+System.Console.WriteLine();
+Iphone meuIPhone = new Iphone("22132213", "Iphone 12", "43546576574", 64);
+meuIPhone.InstalarAplicativo("Tinder");
+
+System.Console.WriteLine();
+
+System.Console.WriteLine("================= Meu Nokia ====================");
+System.Console.WriteLine();
+Nokia meuNokia = new Nokia("40028922", "Nokia Tijolão", "12342313414", 32);
+meuNokia.InstalarAplicativo("Snake");
